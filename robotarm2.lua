d0ec166a440db8593aa42d7cc1a2367289d09b61
Nico <99028112@mydavinci.nl>
2016-09-12 14:24:40 +0200

    no message

---
+++ robotarm2.lua
@@ -0,0 +1,24 @@
+require 'robot_arm'
+robot_arm:load_level('exercise 2')
+robot_arm.speed =  0.95
+robot_arm: grab()
+for i = 1,9 do
+robot_arm: move_right ()
+end
+robot_arm: drop()
+for i = 2,3 do
+robot_arm: move_left()
+end
+robot_arm: grab()
+for i = 1,2 do
+robot_arm: move_right()
+end
+robot_arm: drop()
+for i = 1,5 do
+robot_arm: move_left()
+end
+robot_arm:grab ()
+for i = 1,5 do
+robot_arm: move_right()
+end
+robot_arm: drop()
---
+++ robotarm4.lua
@@ -0,0 +1,22 @@
+require 'robot_arm'
+robot_arm:load_level('exercise 4')
+robot_arm.speed =  0.95
+
+for i = 1,3 do
+robot_arm: grab()
+robot_arm: move_right()
+robot_arm: move_right()
+robot_arm: drop()
+robot_arm: move_left()
+robot_arm: move_left()
+end
+
+robot_arm: move_right()
+robot_arm: move_right()
+
+for i = 1,3 do
+robot_arm: grab()
+robot_arm: move_left()
+robot_arm: drop()
+robot_arm: move_right()
+end
